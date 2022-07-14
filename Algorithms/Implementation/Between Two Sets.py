87b63bfde9298db6bcb4f7744d645a89d3a34622
Vasli Saidmuradov <vasli.saidmuradov@kaspi.kz>
2022-07-14 21:16:49 +0600

    feat: Between Two Sets - python

---
+++ Algorithms/Implementation/Between Two Sets.py
@@ -0,0 +1,20 @@
+# Between Two Sets
+# https://www.hackerrank.com/challenges/between-two-sets/problem
+
+# 1
+def getTotalX(a, b):
+  start, end = a[len(a) - 1], b[0]
+  count = 0
+
+  for i in range(start, end + 1):
+    a_pass, b_pass = True, True
+    for num in a:
+      if i % num != 0:
+        a_pass = False
+    for num in b:
+      if num % i != 0:
+        b_pass = False
+    if a_pass and b_pass:
+      count += 1
+
+  return count
