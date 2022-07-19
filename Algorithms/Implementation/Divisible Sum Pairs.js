bcf6ff6f71a353357012ac95f88bc9397847b57c
Vasli Saidmuradov <vasli.saidmuradov@kaspi.kz>
2022-07-19 14:07:32 +0600

    feat: Divisible Sum Pairs - JS

--- Algorithms/Implementation/Divisible Sum Pairs.js
+++ Algorithms/Implementation/Divisible Sum Pairs.js
@@ -3,3 +3,16 @@
 // https://www.hackerrank.com/challenges/divisible-sum-pairs/problem
 
 // #1
+function divisibleSumPairs(n, k, ar) {
+  // Write your code here
+  let count = 0
+  for (let i = 0; i < n-1; i++) {
+    for (let j = i+1; j < n; j++) {
+      let sumOfPairs = ar[i] + ar[j]
+      if (sumOfPairs % k === 0) {
+        count++
+      }
+    }
+  }
+  return count
+}
