6c3e057d3ddf4cf0f18b9563754dd62d89a9a386
Vasli Saidmuradov <vasli.saidmuradov@kaspi.kz>
2022-07-20 10:20:26 +0600

    feat: Bill Division - JS

--- Algorithms/Implementation/Bill Division.js
+++ Algorithms/Implementation/Bill Division.js
@@ -1,3 +1,19 @@
 // Bill Division
 
 // https://www.hackerrank.com/challenges/bon-appetit/problem
+
+// #1
+function bonAppetit(bill, k, b) {
+  // Write your code here
+  let sum = 0
+
+  for (let i = 0; i < bill.length; i++) {
+    if (i !== k) {
+      sum += bill[i]
+    }
+  }
+
+  let a = sum / 2
+
+  console.log(a === b ? 'Bon Appetit' : b - a)
+}
